c5194053711e5f30d3deec2e1e12f285698337a3
Manuel Tinio <mrctinio@gmail.com>
2023-06-04 23:10:49 +0800

    add spec for publications with stats

--- spec/beehiiv/methods/publication_spec.cr
+++ spec/beehiiv/methods/publication_spec.cr
@@ -18,4 +18,15 @@ describe Beehiiv::Publication do
     publication = Beehiiv::Publication.retrieve(client, id).data
     publication.id.should eq(id)
   end
+
+  it "retrieves a single publication associated with your API key with expanded data for stats" do
+    id = "pub_00000000-0000-0000-0000-000000000000"
+    WebMock.stub(:get, "https://api.beehiiv.com/v2/publications/#{id}")
+      .to_return(status: 200, body: File.read("spec/support/publications_show_with_stats.json"), headers: {"Content-Type" => "application/json"})
+
+    client = Beehiiv.client
+    publication_stats = Beehiiv::Publication.retrieve(client, id).data.stats
+
+    publication_stats.not_nil!.active_subscriptions.should eq(100)
+  end
 end
