acf767423fde53edd1c345fc21da2844aa791adb
Manuel Tinio <mrctinio@gmail.com>
2023-06-04 23:11:01 +0800

    try using HTTP::Params

--- src/beehiiv/methods/retrieve.cr
+++ src/beehiiv/methods/retrieve.cr
@@ -5,7 +5,10 @@ module BeehiivMethods
 {% begin %}
   def self.retrieve(client : HTTP::Client, publication_id : String, {{*arguments}}) : Object({{@type.id}})
     body = { "expand" => "stats" }.to_json
-    response = client.get("/v2/publications/#{publication_id}/#{"{{@type.id.gsub(/Beehiiv::/, "").underscore.gsub(/::/, "/")}}"}s/#{id}", body: body)
+    params = HTTP::Params.build do |params|
+      params["expand"] = "stats"
+    end
+    response = client.get("/v2/publications/#{publication_id}/#{"{{@type.id.gsub(/Beehiiv::/, "").underscore.gsub(/::/, "/")}}"}s/#{id}", params: params)
 
     if response.status_code == 200
       Object({{@type.id}}).from_json(response.body)
